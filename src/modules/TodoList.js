1de7273269ed4347cf2f8dc771ae02b9de31c259
damcio <d.p.koziol@gmail.com>
2023-05-14 22:48:36 +0200

    TodoList.js module added.

---
+++ src/modules/TodoList.js
@@ -0,0 +1,8 @@
+import Task from "./Task";
+import UI from "./UI";
+
+export default class TodoList {
+    const allQuestsArr = [];
+    const todaysQuestsArr = [];
+    const thisWeekQuestsArr = [];
+}
