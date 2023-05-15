f1d9ddce939d26c7afec05be9dc4eae28aa7361b
damcio <d.p.koziol@gmail.com>
2023-05-15 23:49:34 +0200

    More than one task add function updated

--- src/modules/TodoList.js
+++ src/modules/TodoList.js
@@ -2,10 +2,11 @@ import Task from "./Task";
 import UI from "./UI";
 
 export default class TodoList {
+  static tasksArr = [];
+
   static addTask(taskName, taskDate, taskImportancy) {
-    const tasksArr = [];
     const task = new Task(taskName, taskDate, taskImportancy);
-    tasksArr.push(task);
-    console.log(tasksArr);
+    this.tasksArr.push(task);
+    console.log(this.tasksArr);
   }
 }
